357036c2fb66f6744562b2911734cb9fe5f2d84f
Beend Tech <dev@beend.tech>
2026-07-27 10:44:30 -0300

    fix: cache-control no-cache no index.html para forçar sempre buscar nova versao do servidor

--- server.ts
+++ server.ts
@@ -2470,8 +2470,15 @@ async function startServer() {
     app.use(vite.middlewares);
   } else {
     const distPath = path.join(process.cwd(), "dist");
-    app.use(express.static(distPath));
+    app.use(express.static(distPath, {
+      setHeaders: (res, filePath) => {
+        if (filePath.endsWith('.html')) {
+          res.setHeader('Cache-Control', 'no-cache, must-revalidate, proxy-revalidate');
+        }
+      }
+    }));
     app.get("*", (req, res) => {
+      res.setHeader('Cache-Control', 'no-cache, must-revalidate, proxy-revalidate');
       res.sendFile(path.join(distPath, "index.html"));
     });
   }
